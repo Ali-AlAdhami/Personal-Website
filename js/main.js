46dbdc9c7521d5d597a1080196851793091d694f
Ali Raad <aliraad@utexas.edu>
2021-10-02 20:26:14 -0500

    scrolling is now animated through AOS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function() {
+    window.addEventListener('load', () => {
+        AOS.init({
+          duration: 1000,
+          easing: "ease-in-out",
+          once: true,
+          mirror: false
+        });
+      });
+})()
